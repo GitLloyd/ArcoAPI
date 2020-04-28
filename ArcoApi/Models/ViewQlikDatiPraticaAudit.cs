c13ea2bf403d1a79930eca20781f2cf9bb77b55c
GitLloyd <valelloyd@gmail.com>
2020-04-28 17:26:46 +0200

    Corretta mappatura ViewQlikDatiPraticaAudit

--- ArcoApi/Models/ViewQlikDatiPraticaAudit.cs
+++ ArcoApi/Models/ViewQlikDatiPraticaAudit.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ArcoApi.Models
 {
     public partial class ViewQlikDatiPraticaAudit
     {
-        public double? Progressivo { get; set; }
-        public double? IdPratica { get; set; }
-        public double? PraticaEliminata { get; set; }
-        public double? IdGruppo { get; set; }
-        public string NomeGruppo { get; set; }
-        public string DescrizioneGruppo { get; set; }
-        public string EsitoDettaglio { get; set; }
-        public string EsitoGruppo { get; set; }
-        public double? IdAuditOperativo { get; set; }
-        public double? RatingGruppo { get; set; }
+        public string Audit               { get; set; }
+        public string AmbitoAudit         { get; set; }
+        public string TipologiaAudit      { get; set; }
+        public string Stato               { get; set; }
+        public double? ProgressivoPratica    { get; set; }
+        [Column("Numero Caso")]
+        public string NumeroCaso          { get; set; }
+        [Column("Codice Sede")]
+        public string CodiceSede          { get; set; }
+        public string Infortunio          { get; set; }
+        public string Sede                { get; set; }
+        public string Pratica             { get; set; }
+        public string Tipologia           { get; set; }
+        [Column("Numero pratica")]
+        public string NumeroPratica       { get; set; }
+        [Column("N° Progr. progetto")]
+        public string NumeroProgrProgetto { get; set; }
+        [Column("Ragione sociale")]
+        public string RagioneSociale      { get; set; }
+        [Column("Codice ditta")]
+        public string CodiceDitta         { get; set; }
+        public string Ditta               { get; set; }
+        public string Matricola           { get; set; }
+        [Column("N. Caso fornitura")]
+        public string NumeroCasoFornitura { get; set; }
+        [Column("Numero Domanda")]
+        public string NumeroDomanda       { get; set; }
     }
 }
